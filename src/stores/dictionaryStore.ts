a0b98395a289147b8f5e9cda5f86398c74d3277d
Mike (sudomakecoffee) <brisk.home1381@sudomakecoffee.io>
2022-03-15 14:55:39 -0400

    Update test script name, add test for dictionary store

--- src/stores/dictionaryStore.ts
+++ src/stores/dictionaryStore.ts
@@ -1,3 +1,15 @@
 import { defineStore } from "pinia";
+import dictionaryData from "../assets/json/dictionary.json";
 
-export const useDictionaryStore = defineStore("dictionary", {});
+export const useDictionaryStore = defineStore("dictionary", {
+  state: () => {
+    return {
+      dictionary: dictionaryData,
+    };
+  },
+  getters: {
+    valid: (state) => {
+      return (word: string) => state.dictionary.includes(word.toLowerCase());
+    },
+  },
+});

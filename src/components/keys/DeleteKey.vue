4b4d5e3bceafe9b399767d0b5912f852e26b4141
Mike (sudomakecoffee) <brisk.home1381@sudomakecoffee.io>
2022-03-15 12:00:00 -0400

    Update special key styling, add scoped style for svg

--- src/components/keys/DeleteKey.vue
+++ src/components/keys/DeleteKey.vue
@@ -1,9 +1,14 @@
 <template>
-  <button class="key" data-delete><DeleteIcon /></button>
+  <button class="key key-large" data-delete><DeleteIcon /></button>
 </template>
 
 <script setup lang="ts">
 import DeleteIcon from "@/components/icons/IconDelete.vue";
 </script>
 
-<style></style>
+<style scoped>
+.key > svg {
+  width: 1.75em;
+  height: 1.75em;
+}
+</style>

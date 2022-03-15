ceaec2b42736b13d551437658cb98980b2662eff
Mike (sudomakecoffee) <brisk.home1381@sudomakecoffee.io>
2022-03-15 14:56:13 -0400

    Move away from script setup, used to all the boilerplate for now

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,26 @@
   </main>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import TheKeyboard from "@/components/TheKeyboard.vue";
 import WordGrid from "@/components/WordGrid.vue";
+import { defineComponent } from "vue";
+import { useDictionaryStore } from "./stores/dictionaryStore";
+
+export default defineComponent({
+  setup() {
+    const dictionaryStore = useDictionaryStore();
+    const validWord = dictionaryStore.valid;
+
+    return {
+      validWord,
+    };
+  },
+  components: {
+    TheKeyboard,
+    WordGrid,
+  },
+});
 </script>
 
 <style>

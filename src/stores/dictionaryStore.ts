f2ec7105d01b5ecbef7d0f3017ee74e7dd6eff9d
sudomakecoffee <brisk.home1381@sudomakecoffee.io>
2022-03-16 05:23:15 -0400

    Add HMR support

--- src/stores/dictionaryStore.ts
+++ src/stores/dictionaryStore.ts
@@ -1,15 +1,18 @@
-import { defineStore } from "pinia";
+import { acceptHMRUpdate, defineStore } from "pinia";
 import dictionaryData from "@/assets/json/dictionary.json";
 
 export const useDictionaryStore = defineStore("dictionary", {
-  state: () => {
-    return {
-      dictionary: dictionaryData,
-    };
-  },
+  state: () => ({
+    dictionary: dictionaryData,
+  }),
   getters: {
     valid: (state) => {
       return (word: string) => state.dictionary.includes(word.toLowerCase());
     },
   },
 });
+
+// Allow how-reloading
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useDictionaryStore, import.meta.hot));
+}

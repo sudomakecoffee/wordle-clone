bf89caf2b550b778e78e1b079dc94d3f9386230b
sudomakecoffee <brisk.home1381@sudomakecoffee.io>
2022-03-16 16:22:17 -0400

    Refactor bus event type, separate bus types

--- src/use/useGameBus.ts
+++ src/use/useGameBus.ts
@@ -1,10 +1,22 @@
 import type { EventBusKey } from "@vueuse/core";
 
-export const gameBusKey: EventBusKey<{ name: "game" }> = Symbol("symbol-key");
+// Event bus key (queue) for listening to keyboard events
+export const keyboardBusKey: EventBusKey<{ name: "keyboard" }> =
+  Symbol("symbol-key");
 
-export type GameBusEventType = "keypress" | "submit" | "delete";
+// Event bus key (queue) for listening to alert events
+export const alertBusKey: EventBusKey<{ name: "alert" }> = Symbol("symbol-key");
 
+// Event type being emitted/handled
+export enum GameBusEventTypeEnum {
+  keypress = "keypress",
+  submit = "submit",
+  delete = "delete",
+  alert = "alert",
+}
+
+// Event data being emitted/handled
 export interface GameBusData {
-  eventType?: GameBusEventType;
-  data?: string;
+  eventType?: GameBusEventTypeEnum;
+  data?: never;
 }
